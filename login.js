bfb6936998991e72427f0463fbe92af6b18eec5e
Tweagan11 <123520140+Tweagan11@users.noreply.github.com>
2023-03-06 23:51:17 -0700

    Tried running Javascript login

---
+++ login.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(name) {
+        this.username = name;
+        this.prevScore = 0;
+    }
+}
+
+const user1 = new User(Tweag);
+
+const users = [user1]
+
+
+
+const userCheck = new Promise((resolve, reject) => {(name) {
+        if(users.find(name => ))
+        };
+    }
+})
