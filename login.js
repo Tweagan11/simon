742f12ff7d4da4e903db1f5040f792b9ac04547c
Tweagan11 <123520140+Tweagan11@users.noreply.github.com>
2023-03-07 00:15:19 -0700

    Added Scores js

--- login.js
+++ login.js
@@ -1,18 +1,26 @@
-class User {
-    constructor(name) {
-        this.username = name;
-        this.prevScore = 0;
-    }
-}
+// class User {
+//     constructor(name) {
+//         this.username = name;
+//         this.prevScore = 0;
+//     }
+// }
 
-const user1 = new User(Tweag);
+// const user1 = new User(Tweag);
 
-const users = [user1]
+// const users = [user1]
 
 
 
-const userCheck = new Promise((resolve, reject) => {(name) {
-        if(users.find(name => ))
-        };
-    }
-})
+// const userCheck = new Promise((resolve, reject) => {(name) {
+//         if(users.find(({username}) => username === name)) {
+            
+//         }
+//         }
+//     }
+// })
+
+function login() {
+    const nameEl = document.querySelector("#name");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "play.html";
+  }
