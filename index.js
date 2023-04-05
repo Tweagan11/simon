287418b3151f79d6a73b0b7ea033825bb4e3a9d1
Tweagan11 <123520140+Tweagan11@users.noreply.github.com>
2023-04-04 21:17:19 -0600

    Updated index file

--- index.js
+++ index.js
@@ -1,28 +1,87 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const DB = require('./database');
 const app = express();
 
-// The service port. In production the application is statically hosted by the service on the same port.
+const authCookieName = 'token';
+
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-// JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the application's static content
 app.use(express.static('public'));
 
-// Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
+apiRouter.post('/auth/create', async(req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing User' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+apiRouter.post('/auth/login', async(req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if(user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end;
+});
+
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+secureApiRouter.get('/scores', async (req, res) => {
+  const scores = await DB.getHighScores();
   res.send(scores);
 });
 
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
+secureApiRouter.post('/score', async (req, res) => {
+  await DB.addScore(req.body);
+  const scores = await DB.getHighScores();
   res.send(scores);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
 });
 
 // Return the application's default page if the path is unknown
@@ -30,30 +89,38 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+};
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 
 
-let scores = [];
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
+// let scores = [];
+// function updateScores(newScore, scores) {
+//   let found = false;
+//   for (const [i, prevScore] of scores.entries()) {
+//     if (newScore.score > prevScore.score) {
+//       scores.splice(i, 0, newScore);
+//       found = true;
+//       break;
+//     }
+//   }
 
-  if (!found) {
-    scores.push(newScore);
-  }
+//   if (!found) {
+//     scores.push(newScore);
+//   }
 
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
+//   if (scores.length > 10) {
+//     scores.length = 10;
+//   }
 
-  return scores;
-}
+//   return scores;
+// }
